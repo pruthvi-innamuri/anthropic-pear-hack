236e48cc1ba1c665f90922e48bfaa882095bf7d6
Pruthvi Innamuri <pruthvi.innamuri@gmail.com>
2025-04-26 13:24:39 -0700

    getting started

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,34 @@ import express, { Express, Request, Response } from 'express';
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+interface UserQueryState {
+  location: string;
+  cuisine: string;
+  specificFoodItems: string;
+  priceRange: string;
+  distance: string;
+  time: string;
+}
+interface RestaurantCandidate {
+  restaurants: Restaurant[];
+}
+
+interface TimeWindow {
+  start: string;
+  end: string;
+}
+
+interface Restaurant {
+  address: string;
+  cuisine: string;
+  priceRange: string;
+  rating: string;
+  distance: string;
+  time: TimeWindow[];
+  menuItems: string[];
+}
+
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
@@ -10,3 +38,5 @@ app.get('/', (req: Request, res: Response) => {
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 }); 
+
+
